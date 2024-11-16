6972ac1a498f395009a4ef8c15396d73da8827a0
mesyatsevp <mesyatsevp@gmail.com>
2024-11-17 00:56:32 +0300

    Update test_pokemons.py

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -1,22 +1,22 @@
 import requests
 import pytest
 
-# Дефолтные значения
 URL = "https://api.pokemonbattle.ru/v2"
 TOKEN = "d8e2ddfcd39229fceebfcb66a27cdfb2"
 HEADER = {'Content-Type' : "application/json", 'trainer_token' : TOKEN}
 TRAINER_ID = "17718"
 
-@pytest.mark.parametrize('endpoint, expected_status', [('/trainers', 200)])
+def test_status_code():
+    response = requests.get (url = f'{URL}/pokemons', params = {'trainser_id' : TRAINER_ID})
+    assert response.status_code == 200
 
-def test_get_status_code (endpoint, expected_status): # Тест на корректный статус ответа
-    response = requests.get (f'{URL}{endpoint}', params = {'trainer_id': TRAINER_ID})
-    assert response.status_code == expected_status
+def test_part_of_respinse():
+    response_get = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID})
+    assert response_get.json()['data'][0]['name'] == 'New Name'
 
-def test_trainer_name_in_response (): # Тест на то, что в ответе приходит строчка с именем нужного тренера
-    response = requests.get(f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
-    response_data = response.json()
-    assert TRAINER_ID in response.text
-
+@pytest.mark.parametrize ('key, value', [('name', 'New Name'), ('trainer_id', TRAINER_ID), ('id', '135456')])
+def test_parametrize (key, value):
+    response_parametrize = requests.get (url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID})
+    assert response_parametrize.json()['data'][0][key] == value
 
 
